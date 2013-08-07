28dfe8dc1aab6923bb618f9ef91e6de5a870c868
Ty Alexander <ty.alexander@gmail.com>
2013-08-07 09:53:43 -0600

    Separated the natural ordering of time stamped tokens and the ordering the priority queue with a priority comparator

--- coding/src/TokenPrioritizerImpl.java
+++ coding/src/TokenPrioritizerImpl.java
@@ -1,11 +1,11 @@
 import java.util.Comparator;
-import java.util.Date;
 import java.util.PriorityQueue;
 
 public class TokenPrioritizerImpl implements TokenPrioritizer {
 
 	/**
-	 * The TimeStampedToken class wraps a token adding a time stamp used for sorting
+	 * The TimeStampedToken class wraps a token adding a
+	 * nanosecond timestamp used for natural ordering
 	 */
 	private static class TimeStampedToken implements Comparable<TimeStampedToken> {
 		private long time;
@@ -16,44 +16,86 @@ public class TokenPrioritizerImpl implements TokenPrioritizer {
 			this.token = token;
 		}
 
+		/**
+		 * Compare two time stamped tokens.  Tokens are first compared
+		 * by priority and secondly by a nanosecond timestamp
+		 * @param tokenWrapper
+		 * @return
+		 */
 		@Override
 		public int compareTo(TimeStampedToken tokenWrapper) {
-			if (this.token.getPriority() < tokenWrapper.token.getPriority()) {
+			if (this.time < tokenWrapper.time) {
 				return -1;
-			} else if (this.token.getPriority() > tokenWrapper.token.getPriority()) {
+			} else if (this.time > tokenWrapper.time) {
 				return 1;
 			} else {
-				if (this.time < tokenWrapper.time) {
-					return -1;
-				} else if (this.time > tokenWrapper.time) {
-					return 1;
-				} else {
-					throw new RuntimeException("Unable to process non unique tokens");
-				}
+				throw new RuntimeException("Unable to compare non unique time stamped tokens");
 			}
 		}
 
-		/*
-		 * print token id
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == this) {
+				return true;
+			}
+			if (obj == null || obj.getClass() != this.getClass()) {
+				return false;
+			}
+
+			TimeStampedToken timeStampedToken = (TimeStampedToken) obj;
+			return this.time == timeStampedToken.time;
+		}
+
+		/**
+		 * Convert to string of token id and priority
+		 * @return
 		 */
 		@Override
 		public String toString() {
-
 			return "(id:" + token.getTokenID() + ", p:" + String.valueOf(token.getPriority()) + ")";
 		}
 	}
 
-	private final PriorityQueue<TimeStampedToken> queue = new PriorityQueue<TimeStampedToken>();
+	public static class PriorityComparator implements Comparator<TimeStampedToken> {
 
+		@Override
+		public int compare(TimeStampedToken first, TimeStampedToken second) {
+			if (first.token.getPriority() < second.token.getPriority()) {
+				return -1;
+			} else if (first.token.getPriority() > second.token.getPriority()) {
+				return 1;
+			} else {
+				return first.compareTo(second);
+			}
+		}
+	}
+
+	private final PriorityComparator priorityComparator = new PriorityComparator();
+	private final PriorityQueue<TimeStampedToken> queue = new PriorityQueue<TimeStampedToken>(10, priorityComparator);
+
+	/**
+	 * Synchronized function to return the next available token from prioritizer.
+	 * If no tokens are available in prioritizer null is returned.
+	 * @return
+	 */
 	public synchronized Token nextToken() {
 		TimeStampedToken timeStampedToken = queue.poll();
 		return (timeStampedToken != null ) ? timeStampedToken.token : null;
 	}
 
+	/**
+	 * Synchronized method to add a token to the prioritizer.  Assumes
+	 * token is well-formed and validated.  Assumes token is unique.
+	 * @param theToken
+	 */
 	public synchronized void addToken(Token theToken) {
 		queue.offer(new TimeStampedToken(System.nanoTime(), theToken));
 	}
 
+	/**
+	 * Convert prioritized items into strings
+	 * @return
+	 */
 	@Override
 	public String toString() {
 		StringBuffer stringBuffer = new StringBuffer();
